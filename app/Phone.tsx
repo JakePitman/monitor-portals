63f3f09ca6c56a4aa5c4a1ff81d11013798a22fa
Jake Pitman <jakeseanp@gmail.com>
2023-09-02 23:59:14 +1200

    Add buttons

--- app/Phone.tsx
+++ app/Phone.tsx
@@ -17,9 +17,19 @@ export const Phone = ({ position, renderPortal }: Props) => {
   const phoneDepth = 0.5;
   return (
     <group position={position} ref={ref}>
+      <mesh position={[-2.5, 3, 0]} scale={0.1}>
+        <RoundedBox args={[1.5, 6, 1.5]} radius={0.3}>
+          <meshBasicMaterial color="black" toneMapped={false} />
+        </RoundedBox>
+      </mesh>
+      <mesh position={[-2.5, 2, 0]} scale={0.1}>
+        <RoundedBox args={[1.5, 6, 1.5]} radius={0.3}>
+          <meshBasicMaterial color="black" toneMapped={false} />
+        </RoundedBox>
+      </mesh>
       <mesh>
         <RoundedBox args={[5, 10, phoneDepth]} radius={0.5}>
-          <meshStandardMaterial color="black" toneMapped={false} />
+          <meshBasicMaterial color="black" toneMapped={false} />
         </RoundedBox>
       </mesh>
       {renderPortal(0.2)}
