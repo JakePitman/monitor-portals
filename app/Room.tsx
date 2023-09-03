19d17382b9f397881bd17be4267a2ffe42cfa319
Jake Pitman <jakeseanp@gmail.com>
2023-09-04 11:52:01 +1200

    Add lamp

--- app/Room.tsx
+++ app/Room.tsx
@@ -9,8 +9,25 @@ const Desk = () => (
   </mesh>
 );
 
-const Lamp = () => {
-  return <></>;
+const coneGeometry = new THREE.ConeGeometry(1.5, 3, 15, 1, true);
+const lampMaterial = new THREE.MeshStandardMaterial({
+  color: "red",
+  side: THREE.DoubleSide,
+});
+type LampProps = {
+  position: [number, number, number];
+  rotation: [number, number, number];
+};
+const Lamp = ({ position, rotation }: LampProps) => {
+  return (
+    <group position={position} rotation={rotation}>
+      {/*
+      TODO: Add this in and make shadows work
+      <pointLight intensity={5} position={[0, -1, 0]} castShadow />
+      */}
+      <mesh geometry={coneGeometry} material={lampMaterial}></mesh>
+    </group>
+  );
 };
 
 const wallMaterial = new THREE.MeshStandardMaterial({
@@ -39,6 +56,7 @@ export const Room = () => {
       <Wall dimensions={[35, 40, 1]} position={[0, -5, -10]} />
       <Wall dimensions={[1, 40, 35]} position={[-17.5, -5, 0]} />
       <Wall dimensions={[1, 40, 35]} position={[17.5, -5, 0]} />
+      <Lamp position={[16, 8, 8]} rotation={[0.8, 0, -0.4]} />
       <Desk />
     </group>
   );
