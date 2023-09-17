524dca21c043b6355450f727c3cacc6d55e4fea2
jakepitman <jakeseanp@gmail.com>
2023-09-17 22:32:33 +1200

    Offset triangles in message

--- app/Message.tsx
+++ app/Message.tsx
@@ -1,4 +1,4 @@
-import { Text, RoundedBox } from "@react-three/drei";
+import { Text, RoundedBox, Float } from "@react-three/drei";
 import { MeshStandardMaterial } from "three";
 
 const messageMaterial = new MeshStandardMaterial({ color: "white" });
@@ -12,25 +12,28 @@ type Props = {
 export const Message = ({ text, iteration }: Props) => {
   const height = 1;
   const yOffset = iteration * (height - 0.2);
+  const triangleXPosition = iteration % 2 === 0 ? -1.5 : 1.5;
   return (
-    <group position={[1.8, 8 - yOffset, 0.5]} scale={0.5}>
-      <RoundedBox
-        material={messageMaterial}
-        args={[5, height, messageDepth]}
-        radius={0.15}
-      >
-        <Text position={[0, 0, 0.2]} scale={0.5} color={"black"}>
-          {text}
-        </Text>
-      </RoundedBox>
+    <Float floatIntensity={0} rotationIntensity={0}>
+      <group position={[1.8, 8 - yOffset, 0.5]} scale={0.5}>
+        <RoundedBox
+          material={messageMaterial}
+          args={[5, height, messageDepth]}
+          radius={0.2}
+        >
+          <Text position={[0, 0, 0.2]} scale={0.5} color={"black"}>
+            {text}
+          </Text>
+        </RoundedBox>
 
-      <mesh
-        material={messageMaterial}
-        position={[0, -0.2, 0]}
-        rotation={[0, 0, Math.PI * 0.23]}
-      >
-        <boxGeometry args={[0.8, 0.8, messageDepth]} />
-      </mesh>
-    </group>
+        <mesh
+          material={messageMaterial}
+          position={[triangleXPosition, -0.2, 0]}
+          rotation={[0, 0, Math.PI * 0.23]}
+        >
+          <boxGeometry args={[0.8, 0.8, messageDepth]} />
+        </mesh>
+      </group>
+    </Float>
   );
 };
