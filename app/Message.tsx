e0b2e746460397bc29bbe0572e1a2ba2b55d22c5
jakepitman <jakeseanp@gmail.com>
2023-09-17 23:09:33 +1200

    Remove float

--- app/Message.tsx
+++ app/Message.tsx
@@ -1,6 +1,6 @@
 import { useRef } from "react";
 import { useFrame } from "@react-three/fiber";
-import { Text, RoundedBox, Float } from "@react-three/drei";
+import { Text, RoundedBox } from "@react-three/drei";
 import { MeshStandardMaterial } from "three";
 import { easing } from "maath";
 
@@ -18,8 +18,6 @@ export const Message = ({ text, iteration, isHovered }: Props) => {
   const yOffset = iteration * (height - 0.2);
   const zOffset = { idle: 0.5, hovered: 3 };
   const triangleXPosition = iteration % 2 === 0 ? -1.5 : 1.5;
-  //const floatIntensity = isHovered ? 1 : 0;
-  const floatIntensity = 0;
   const groupRef = useRef(null);
   const yRotation = { idle: 0, hovered: iteration % 2 === 0 ? -0.7 : 0.7 };
   const xOffset = { idle: 1.8, hovered: iteration % 2 === 0 ? 4 : -0 };
@@ -61,30 +59,28 @@ export const Message = ({ text, iteration, isHovered }: Props) => {
   });
 
   return (
-    <Float floatIntensity={floatIntensity} rotationIntensity={floatIntensity}>
-      <group
-        position={[xOffset.idle, 8 - yOffset, zOffset.idle]}
-        scale={0.5}
-        ref={groupRef}
+    <group
+      position={[xOffset.idle, 8 - yOffset, zOffset.idle]}
+      scale={0.5}
+      ref={groupRef}
+    >
+      <RoundedBox
+        material={messageMaterial}
+        args={[5, height, messageDepth]}
+        radius={0.2}
       >
-        <RoundedBox
-          material={messageMaterial}
-          args={[5, height, messageDepth]}
-          radius={0.2}
-        >
-          <Text position={[0, 0, 0.2]} scale={0.5} color={"black"}>
-            {text}
-          </Text>
-        </RoundedBox>
+        <Text position={[0, 0, 0.2]} scale={0.5} color={"black"}>
+          {text}
+        </Text>
+      </RoundedBox>
 
-        <mesh
-          material={messageMaterial}
-          position={[triangleXPosition, -0.2, 0]}
-          rotation={[0, 0, Math.PI * 0.23]}
-        >
-          <boxGeometry args={[0.8, 0.8, messageDepth]} />
-        </mesh>
-      </group>
-    </Float>
+      <mesh
+        material={messageMaterial}
+        position={[triangleXPosition, -0.2, 0]}
+        rotation={[0, 0, Math.PI * 0.23]}
+      >
+        <boxGeometry args={[0.8, 0.8, messageDepth]} />
+      </mesh>
+    </group>
   );
 };
